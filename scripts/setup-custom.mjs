81e770db17f79fb22d05467701994ef8abcde602
Kent C. Dodds <me+github@kentcdodds.com>
2023-03-18 13:18:28 -0600

    fix setup script and update stuff

--- scripts/setup-custom.mjs
+++ scripts/setup-custom.mjs
@@ -1,63 +1,67 @@
 import fs from 'fs'
 import path from 'path'
+import os from 'os'
 import { spawn } from 'child_process'
 import fsExtra from 'fs-extra'
+import { $ } from 'execa'
+import pMap from 'p-map'
 import {
 	getApps,
-	getProblemApps,
-	getSolutionApps,
+	isProblemApp,
 } from '@kentcdodds/workshop-app/build/utils/apps.server.js'
 
 // getApps expects this env var
 process.env.NODE_ENV = 'development'
 
-const appPaths = new Set((await getApps()).map(a => a.fullPath))
+const allApps = await getApps()
+const uniqueApps = allApps.filter(
+	(a, index) => allApps.findIndex(b => b.fullPath === a.fullPath) === index,
+)
 
 console.log(
 	'📝  copying .env.example to .env files and generating prisma client...',
 )
-for (const appPath of appPaths) {
-	await fs.promises.copyFile(
-		path.join(appPath, '.env.example'),
-		path.join(appPath, '.env'),
-	)
-	const cp = spawn('npx', ['prisma', 'generate'], {
-		cwd: appPath,
-		stdio: 'inherit',
-		shell: true,
-	})
-	await new Promise(res => {
-		cp.on('exit', code => {
-			if (code === 0) {
-			} else {
-				console.error(`❌  generating the prisma client failed in ${appPath}`)
-				process.exit(1)
-			}
-			res()
-		})
-	})
-}
 
-const problemApps = await getProblemApps()
+const problemApps = allApps.filter(isProblemApp)
 
 // we just grab the last problem app and set that one up because we're setup to
 // have all the exercise apps share a single database and prisma has a
 // postinstall that sets up the client in each individual app anyway.
 const lastProblemApp = problemApps[problemApps.length - 1]
-console.log(`🛠️  Setting up ${lastProblemApp.name}...`)
-const cp = spawn('npm', ['run', 'setup', '--silent'], {
-	cwd: lastProblemApp.fullPath,
-	stdio: 'inherit',
-	shell: true,
-})
+await pMap(
+	uniqueApps,
+	async app => {
+		await fs.promises.copyFile(
+			path.join(app.fullPath, '.env.example'),
+			path.join(app.fullPath, '.env'),
+		)
 
-const code = await new Promise(res => {
-	cp.on('exit', code => {
-		res(code)
-	})
-})
+		if (app.fullPath === lastProblemApp.fullPath) {
+			const { all, exitCode } = await $({
+				cwd: lastProblemApp.fullPath,
+				all: true,
+			})`npm run setup --silent`
+			if (exitCode === 0) {
+				console.log(`✅  Setup complete for ${lastProblemApp.relativePath}`)
+			} else {
+				console.log(all)
+				throw new Error(
+					`❌  npm run setup for ${lastProblemApp.relativePath} app failed`,
+				)
+			}
+		} else {
+			const { all, exitCode } = await $({
+				cwd: app.fullPath,
+				all: true,
+			})`npx prisma generate`
 
-if (code !== 0) {
-	console.error(`❌  Setting up ${lastProblemApp.name} failed`)
-	process.exit(code)
-}
+			if (exitCode === 0) {
+				console.log(`✅  prisma client generated for ${app.relativePath}`)
+			} else {
+				console.log(all)
+				throw new Error(`❌  prisma generate failed for ${app.relativePath}`)
+			}
+		}
+	},
+	{ concurrency: Math.ceil(os.cpus().length / 3) },
+)
