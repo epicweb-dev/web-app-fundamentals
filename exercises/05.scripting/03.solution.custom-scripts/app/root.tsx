61cb1cf4dc2a4ac555d34860174979f38cf686da
onemen <tabmix.onemen@gmail.com>
2023-04-01 15:52:09 +0300

    update tailwindcss to v3.3.1, use tailwind-scrollbar plugin for thin scrollbar

--- exercises/05.scripting/03.solution.custom-scripts/app/root.tsx
+++ exercises/05.scripting/03.solution.custom-scripts/app/root.tsx
@@ -63,7 +63,7 @@ export default function App() {
 			<head>
 				<Links />
 			</head>
-			<body className="flex h-full flex-col justify-between bg-night-700 text-white">
+			<body className="flex h-full flex-col justify-between bg-night-700 text-white scrollbar-thin scrollbar-thumb-gray-300">
 				<header className="container mx-auto py-6">
 					<nav className="flex justify-between">
 						<Link to="/">
@@ -133,7 +133,7 @@ function UserDropdown() {
 					to={`/users/${user.username}`}
 					// this is for progressive enhancement
 					onClick={e => e.preventDefault()}
-					className="flex items-center gap-2 rounded-full bg-night-500 py-2 pl-2 pr-4 outline-none hover:bg-night-400 focus:bg-night-400 radix-state-open:bg-night-400"
+					className="flex items-center gap-2 rounded-full bg-night-500 py-2 pl-2 pr-4 outline-none focus:bg-night-400 radix-state-open:bg-night-400 hover:bg-night-400"
 				>
 					<img
 						className="h-8 w-8 rounded-full object-cover"
@@ -154,7 +154,7 @@ function UserDropdown() {
 					<DropdownMenu.Item asChild>
 						<Link
 							to={`/users/${user.username}`}
-							className="rounded-t-3xl py-5 px-7 outline-none hover:bg-night-500 radix-highlighted:bg-night-500"
+							className="rounded-t-3xl py-5 px-7 outline-none radix-highlighted:bg-night-500 hover:bg-night-500"
 						>
 							👤 Profile
 						</Link>
@@ -162,7 +162,7 @@ function UserDropdown() {
 					<DropdownMenu.Item asChild>
 						<Link
 							to="/favorites"
-							className="py-5 px-7 outline-none hover:bg-night-500 radix-highlighted:bg-night-500"
+							className="py-5 px-7 outline-none radix-highlighted:bg-night-500 hover:bg-night-500"
 						>
 							🔖 Favorites
 						</Link>
@@ -170,7 +170,7 @@ function UserDropdown() {
 					<DropdownMenu.Item asChild>
 						<Link
 							to="/bookings"
-							className="py-5 px-7 outline-none hover:bg-night-500 radix-highlighted:bg-night-500"
+							className="py-5 px-7 outline-none radix-highlighted:bg-night-500 hover:bg-night-500"
 						>
 							🚀 Bookings
 						</Link>
